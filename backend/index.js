90ce95fdf21d4b6a05c904d534d5d9ae94b0853a
O V V S Ganapathi <othojuganapathi123@gmail.com>
2025-09-28 21:01:12 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [process.env.FRONTEND_URL,'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 }));
